13d2844dc8d53f1fb8144b221c35f20a6639c6c7
Alexis MEUNIER <alexis.meunier@epita.fr>
2026-06-27 10:28:32 +0200

    feat: No way, a normal constructor ?

--- src/segmentation.py
+++ src/segmentation.py
@@ -1,34 +1,13 @@
 import itk
 
-def tumor_segmentation(image, seed=[110, 100, 50], lower=180, upper=255, multiplier=3, iterations=5,
-                   time_step=0.125):
-    image_type = type(image)
+def tumor_segmentation(image, seed=[110, 100, 50], multiplier=1.5, iterations=5, initial_radius=2):
+    mask = itk.confidence_connected_image_filter(
+        image,
+        seed=seed,
+        multiplier=multiplier,
+        number_of_iterations=iterations,
+        initial_neighborhood_radius=initial_radius,
+        replace_value=1,
+    )
+    return mask
 
-    smoother = itk.GradientAnisotropicDiffusionImageFilter[image_type, image_type].New()
-    smoother.SetInput(image)
-    smoother.SetNumberOfIterations(iterations)
-    smoother.SetTimeStep(time_step)
-    smoother.SetConductanceParameter(multiplier)
-    smoother.Update()
-
-    # # Instantiate the filter
-    connected_threshold = itk.ConnectedThresholdImageFilter[image_type, image_type].New()
-
-    # Configure filter
-    connected_threshold.SetSeed(seed)
-    connected_threshold.SetUpper(upper)
-    connected_threshold.SetLower(lower)
-    connected_threshold.SetInput(smoother.GetOutput())
-
-    dimension = image.GetImageDimension()
-
-    # Execute pipeline
-    connected_threshold.Update()
-
-    in_type = itk.output(connected_threshold)
-    output_type = itk.Image[itk.UC, dimension]
-    rescaler = itk.RescaleIntensityImageFilter[in_type, output_type].New(connected_threshold)
-    rescaler.SetOutputMinimum(0)
-    rescaler.SetOutputMaximum(255)
-
-    return rescaler.GetOutputMaximum()
