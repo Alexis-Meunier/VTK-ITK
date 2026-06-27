4e2d9dd0fe19a1fc3344bb9f5f9b7a96a6f78537
Alexis MEUNIER <alexis.meunier@epita.fr>
2026-06-27 10:06:34 +0200

    fix: Remove matplotlib

--- src/segment.py
+++ src/segment.py
@@ -1,14 +1,9 @@
 import itk
-import matplotlib.pyplot as plt
-
 
 def segment(input_filepath="data/case6_gre1.nrrd",
-         output_filepath='tumor-segmented.png', seedX=110, seedY=100, lower=180, upper=255):
+         output_filepath='tumor-segmented.png', seedX=110, seedY=100, seedZ=50, lower=180, upper=255):
     # Instantiate the reader
     input_image = itk.imread(input_filepath, pixel_type=itk.F)
-    plt.ion()
-    plt.imshow(itk.GetArrayViewFromImage(input_image), cmap="gray")
-    plt.waitforbuttonpress()
 
     smoother = itk.GradientAnisotropicDiffusionImageFilter[type(input_image), type(input_image)].New()
     smoother.SetInput(input_image)
@@ -17,12 +12,6 @@ def segment(input_filepath="data/case6_gre1.nrrd",
     smoother.SetConductanceParameter(3)
     smoother.Update()
 
-
-    # Display image with matplotlib
-    plt.ion()
-    plt.imshow(itk.GetArrayViewFromImage(smoother.GetOutput()) , cmap="gray")
-    plt.waitforbuttonpress()
-    #
     # # Instantiate the filter
     connected_threshold = itk.ConnectedThresholdImageFilter[type(input_image), type(input_image)].New()
 
@@ -33,17 +22,15 @@ def segment(input_filepath="data/case6_gre1.nrrd",
     connected_threshold.SetInput(smoother.GetOutput())
 
     dimension = input_image.GetImageDimension()
-    #
+
     # Execute pipeline
     connected_threshold.Update()
     result = connected_threshold.GetOutput()
-    plt.imshow(itk.GetArrayViewFromImage(result), cmap="gray")
-    plt.waitforbuttonpress()
-    #
+
     in_type = itk.output(connected_threshold)
     output_type = itk.Image[itk.UC, dimension]
     rescaler = itk.RescaleIntensityImageFilter[in_type, output_type].New(connected_threshold)
     rescaler.SetOutputMinimum(0)
     rescaler.SetOutputMaximum(255)
-    #
+
     itk.imwrite(rescaler, output_filepath)
