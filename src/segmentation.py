4bfd0551a202e5ceeb27fc768b6ec672abd267f9
Alexis MEUNIER <alexis.meunier@epita.fr>
2026-06-27 10:45:13 +0200

    feat: Segmentation looks good!

--- src/segmentation.py
+++ src/segmentation.py
@@ -1,6 +1,25 @@
 import itk
 
-def tumor_segmentation(image, seed=[110, 100, 50], multiplier=1.5, iterations=5, initial_radius=2):
+# Hard coded values because subject asked us
+# "Ce script devra s’exécuter sans nécessiter d’interaction utilisateur"
+SEED_SCAN1 = [103, 77, 51]
+SEED_SCAN2 = [91, 89, 53]
+
+# region growing parameters, same for both scans so the two
+# segmentations are produced by the exatc same method
+MULTIPLIER = 1.2
+NUM_ITERATIONS = 2
+RADIUS = 2
+
+
+def tumor_segmentation(image, seed, multiplier=MULTIPLIER, iterations=NUM_ITERATIONS,
+                   initial_radius=RADIUS):
+    """
+    Segment the tumor in `image` starting using a region growing algorithm.
+    The region starts at coordinates `seed`.
+
+    Returns a mask representing the segmented tumor (1 = tumor, 0 = background).
+    """
     mask = itk.confidence_connected_image_filter(
         image,
         seed=seed,
@@ -10,4 +29,3 @@ def tumor_segmentation(image, seed=[110, 100, 50], multiplier=1.5, iterations=5,
         replace_value=1,
     )
     return mask
-
