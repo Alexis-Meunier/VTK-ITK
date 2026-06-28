0ee93c34f7ed7659a8edf3f14fb22a61baacf3b9
anis <anis.feore@epita.fr>
2026-06-28 16:51:03 +0200

    feat: implement estimate_transformation with rigid + affine stages

--- src/registration.py
+++ src/registration.py
@@ -51,3 +51,56 @@ def _check_transform(
     )
 
     return initial_transform
+
+
+def estimate_transformation(fixed, moving):
+    ImageType = type(fixed)
+
+    # 1: Rigit Registration
+    RigidTransformType = itk.VersorRigid3DTransform[itk.D]
+    rigid_transform = RigidTransformType.New()
+
+    initializer = itk.CenteredTransformInitializer[
+        RigidTransformType, ImageType, ImageType
+    ].New()
+    initializer.SetTransform(rigid_transform)
+    initializer.SetFixedImage(fixed)
+    initializer.SetMovingImage(moving)
+    initializer.MomentsOn()
+    initializer.InitializeTransform()
+
+    rigid_transform = _check_transform(
+        fixed,
+        moving,
+        rigid_transform,
+        levels=3,
+        shrink=[4, 2, 1],
+        smooth=[2, 1, 0],
+        learning_rate=1.0,
+        min_step=0.001,
+        iterations=100,
+        stage_name="rigid",
+    )
+
+    # 2: Affine Registration
+    AffineTransformType = itk.AffineTransform[itk.D, 3]
+    affine_transform = AffineTransformType.New()
+    affine_transform.SetIdentity()
+    affine_transform.SetMatrix(rigid_transform.GetMatrix())
+    affine_transform.SetOffset(rigid_transform.GetOffset())
+    affine_transform.SetCenter(rigid_transform.GetCenter())
+
+    affine_transform = _check_transform(
+        fixed,
+        moving,
+        affine_transform,
+        levels=2,
+        shrink=[2, 1],
+        smooth=[1, 0],
+        learning_rate=0.5,
+        min_step=0.0005,
+        iterations=150,
+        stage_name="affine",
+    )
+
+    return affine_transform
